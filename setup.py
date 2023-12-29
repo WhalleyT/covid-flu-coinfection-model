c32880dc656a5037ac9a8238c6a3d6fff07218ac
whalleyt <whalleyt@cardiff.ac.uk>
2023-12-29 11:28:45 +0000

    update install

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+print("Hello")
+print(find_packages())
 
 setup(
     name="covid-influenza-coinfection-model",
     version="1.0",
     packages=find_packages(),
+    install_requires=[
+        "bokeh==3.1.1",
+        "numpy",
+        "scipy"
+        ]
 )
